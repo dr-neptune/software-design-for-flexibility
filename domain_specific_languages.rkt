c114697a485bb859663b9b39a52668ab759e6af0
dr-neptune <mrose4@worcester.edu>
2023-08-04 17:13:13 -0400

    a small library

--- domain_specific_languages.rkt
+++ domain_specific_languages.rkt
@@ -82,3 +82,121 @@
 
 
 ;; Multiple Values
+(define list-head take)
+
+(define (spread-apply f g)
+  (let ([n (get-arity f)])
+    (define (the-combination . args)
+      (values (apply f (list-head args n))
+              (apply g (list-tail args n))))
+    the-combination))
+
+(define (compose f g)
+  (define (the-composition . args)
+    (call-with-values (λ () (apply g args)) f))
+  the-composition)
+
+
+(define (spread-combine h f g)
+  (compose h (spread-apply f g)))
+
+
+((spread-combine list
+                 (λ (x y) (list 'foo x y))
+                 (λ (u v w) (list 'bar u v w)))
+ 'a 'b 'c 'd 'e)
+
+;; side quest
+;; didn't read ahead in the book
+;; there was an arity problem with racket's values
+;; which was resolved by updating compose on the next page
+(define-syntax values->list
+  (syntax-rules ()
+    [(_ body)
+     (call-with-values
+      (λ () body)
+      list)]))
+
+(values->list
+ ((spread-apply (λ (x y) (list 'foo x y))
+                (λ (u v w) (list 'bar u v w))) 'a 'b 'c 'd 'e))
+
+
+(define (spread-apply f g)
+  (let ([n (get-arity f)])
+    (define (the-combination . args)
+      (let-values ([(fv) (apply f (list-head args n))]
+                   [(gv) (apply g (list-tail args n))])
+        (apply values (append fv gv))))
+    the-combination))
+
+
+(values->list
+ ((spread-apply (λ (x) '(foo x))
+                (λ (y) '(bar y))) 'a 'b))
+
+((spread-combine list
+                 (λ (x y) (values x y))
+                 (λ (u v w) (values w v u)))
+ 'a 'b 'c 'd 'e)
+
+;; a small library
+(define (list-remove ls i)
+  (append (take ls i) (drop ls (add1 i))))
+
+(define (list-insert ls v i)
+  (append (take ls i)
+          (list v)
+          (drop ls i)))
+
+(define (list-insert ls i v)
+  (match i
+    [0 (cons v ls)]
+    [_ (cons (car ls) (list-insert (cdr ls) (sub1 i) v))]))
+
+(define (discard-argument i)
+  (λ (f)
+    (let ([m (+ (get-arity f) 1)])
+      (define (the-combination . args)
+        (apply f (list-remove args i)))
+      the-combination)))
+
+(((discard-argument 2)
+  (λ (x y z) (list 'foo x y z)))
+ 'a 'b 'c 'd)
+
+
+(define ((curry-argument i) . args)
+  (λ (f)
+    (λ (x)
+      (apply f (list-insert args i x)))))
+
+
+((((curry-argument 2) 'a 'b 'c)
+  (λ (x y z w) (list 'foo x y z w)))
+ 'd)
+
+
+(define (make-permutation permspec)
+  (define (permuter ls)
+    (map (λ (p) (list-ref ls p)) permspec))
+  permuter)
+
+((make-permutation (list 1 0 3 2)) '(a b c d))
+
+
+(define (permute-arguments . permspec)
+  (let ([permute (make-permutation permspec)])
+    (λ (f)
+      (define (combination . args)
+        (apply f (permute args)))
+      (let ([n (get-arity f)])
+        combination))))
+
+
+(((permute-arguments 1 2 0 3)
+  (λ (x y z w) (list 'foo x y z w)))
+ 'a 'b 'c 'd)
+
+
+;; combinators and body plans
